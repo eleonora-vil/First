da6350da454f202ec526221d53f9d44c440abab7
eleonora-vil <vinhthitheokaka@gmail.com>
2023-09-19 16:05:26 +0700

    update body

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,19 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Body from "./components/Body";
+import Item from "./components/Item";
 function App() {
   return (
     <div className="app">
       <Header />
       <Navbar />
-      <Footer />
       <Body />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Item />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Item.jsx
@@ -0,0 +1,262 @@
+import React from "react";
+import "./Item.css";
+export default function Item() {
+  return (
+    <div id="item-container">
+      <div className="name">
+        <h3>LỒNG CU GÁY</h3>
+      </div>
+      <div className="item-list">
+        <div className="item">
+          <div className="item-img">
+            <img
+              className="img"
+              src="https://longchim.vn/wp-content/uploads/2023/05/long-cu-gay-qua-dao-02-1.jpg"
+            />
+          </div>
+          <div className="item-detail">
+            <div className="title">
+              <b> Lồng cu gáy quả đào 02 đặc biệt</b>
+            </div>
+            <div className="price">
+              <div>2.700.000đ</div>
+              <div>2.000.000đ</div>
+            </div>
+            <table className="table">
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Mã sản phẩm</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">ST001</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Hình dáng</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Quả Đào</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Chất liệu</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Tre già</span>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-img">
+            <img
+              className="img"
+              src="https://longchim.vn/wp-content/uploads/2023/05/long-cu-gay-qua-dao-02-1.jpg"
+            />
+          </div>
+          <div className="item-detail">
+            <div className="title">
+              <b> Lồng cu gáy quả đào 02 đặc biệt</b>
+            </div>
+            <div className="price">
+              <div>2.700.000đ</div>
+              <div>2.000.000đ</div>
+            </div>
+            <table className="table">
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Mã sản phẩm</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">ST001</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Hình dáng</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Quả Đào</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Chất liệu</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Tre già</span>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-img">
+            <img
+              className="img"
+              src="https://longchim.vn/wp-content/uploads/2023/05/long-cu-gay-qua-dao-02-1.jpg"
+            />
+          </div>
+          <div className="item-detail">
+            <div className="title">
+              <b> Lồng cu gáy quả đào 02 đặc biệt</b>
+            </div>
+            <div className="price">
+              <div>2.700.000đ</div>
+              <div>2.000.000đ</div>
+            </div>
+            <table className="table">
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Mã sản phẩm</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">ST001</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Hình dáng</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Quả Đào</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Chất liệu</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Tre già</span>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-img">
+            <img
+              className="img"
+              src="https://longchim.vn/wp-content/uploads/2023/05/long-cu-gay-qua-dao-02-1.jpg"
+            />
+          </div>
+          <div className="item-detail">
+            <div className="title">
+              <b> Lồng cu gáy quả đào 02 đặc biệt</b>
+            </div>
+            <div className="price">
+              <div>2.700.000đ</div>
+              <div>2.000.000đ</div>
+            </div>
+            <table className="table">
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Mã sản phẩm</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">ST001</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Hình dáng</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Quả Đào</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Chất liệu</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Tre già</span>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+        <div className="item">
+          <div className="item-img">
+            <img
+              className="img"
+              src="https://longchim.vn/wp-content/uploads/2023/05/long-cu-gay-qua-dao-02-1.jpg"
+            />
+          </div>
+          <div className="item-detail">
+            <div className="title">
+              <b> Lồng cu gáy quả đào 02 đặc biệt</b>
+            </div>
+            <div className="price">
+              <div>2.700.000đ</div>
+              <div>2.000.000đ</div>
+            </div>
+            <table className="table">
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Mã sản phẩm</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">ST001</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Hình dáng</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Quả Đào</span>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <p className="para">
+                    <b>Chất liệu</b>
+                  </p>
+                </td>
+                <td>
+                  <span className="value">Tre già</span>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </div>
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
