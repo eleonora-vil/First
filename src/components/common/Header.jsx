cc5d3ffad8ffb923a4264d0c71ee95bbe337f6e3
Sang Dang <sangdase170287@fpt.edu.vn>
2023-09-24 17:10:34 +0700

    Change home

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+    return (
+        <div id="header">
+            <section className="header-top">
+                <div className="service">
+                    Chăm sóc khách hàng
+                    <span className="phone"> 0334567856</span>
+                </div>
+                <a href="/Login.jsx" className="login">
+                    Login
+                </a>
+            </section>
+            <section className="header-bottom">
+                <div className="logo">BICA</div>
+                <div className="search-container">
+                    <input className="search-bar" type="text" placeholder="Search" />
+                    <button className="search-button">🔍︎</button>
+                </div>
+                <button className="cart-button"> Cart 🛒</button>
+            </section>
+        </div>
+    )
+}
